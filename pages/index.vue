e43b8fc1f18e4a9fe6eb06a0aab66f24d1fad4bc
Oleg <zizu19988@gmail.com>
2019-08-23 19:55:23 +0300

    (feature/smooth-scroll) Enable smooth scroll at home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,60 @@
-<!-- [pages] About --- component -->
+<!-- [pages] Home --- component -->
 
 <template>
-	<div class="container--p">
-		<h2 class="title">Home page</h2>
+	<div class="content --scroll">
+		<div class="hero">
+			<h2 class="title">Here will be projects</h2>
+		</div>
+		
+		<div class="img"></div>
 	</div>
 </template>
 
 <script>
+	// Vuex
+	import { mapState, mapMutations } from "vuex";
+	
+	// Mixins
 	import Head from '~/mixins/Head';
+	import LifecycleHooks from '~/mixins/LifecycleHooks';
+	import Transitions from '~/mixins/Transitions';
+	
+	// Event dispatcher
+	import { Events, TRANSITION_ENTER_DONE } from "~/assets/js/Events";
 	
 	export default {
-		mixins: [Head],
+		name: 'Index',
+		mixins: [ Head, LifecycleHooks, Transitions ],
+		computed: {
+			...mapState({
+				vertical: state=>state.scroll.verticalScroll
+			})
+		},
+		methods: {
+			...mapMutations({
+				setScrollActive: "scroll/setActive",
+				setScrollDirection: "scroll/setScrollDirection",
+				setScrollTo: "scroll/updateScrollTo"
+			}),
+			
+			// Enable scroll
+			enableScroll() {
+				this.setScrollDirection(this.vertical);
+				this.setScrollActive(true);
+			},
+			
+			/* MOUNTED -------------------------------------------------------------------------------------------------- */
+			addListeners() {
+				this.enterHandler = this.enableScroll.bind(this);
+				Events.addEventListener(TRANSITION_ENTER_DONE, this.enterHandler);
+			},
+			
+			/* BEFORE DESTROY ------------------------------------------------------------------------------------------- */
+			removeListeners() {
+				Events.removeEventListener(TRANSITION_ENTER_DONE, this.enterHandler);
+			},
+		},
 	};
 </script>
 
-<style src="./about.scss" lang="scss" scoped></style>
+<style src="./index.scss" lang="scss" scoped></style>
