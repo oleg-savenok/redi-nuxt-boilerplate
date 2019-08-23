8b65558a6d736ce1b3a992856c2fae60f00bf6d1
Oleg <zizu19988@gmail.com>
2019-08-23 19:56:03 +0300

    (feature/smooth-scroll) Enable smooth scroll at [about, contact] pages

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,17 +1,60 @@
 <!-- [pages] Contact --- component -->
 
 <template>
-	<div class="container--p">
-		<h2 class="title">Contact page</h2>
+	<div class="content --scroll">
+		<div class="hero">
+			<h2 class="title">Contact page</h2>
+		</div>
+		
+		<div class="img"></div>
 	</div>
 </template>
 
 <script>
-import Head from '~/mixins/Head';
-
-export default {
-	mixins: [Head],
-};
+	// Vuex
+	import { mapState, mapMutations } from "vuex";
+	
+	// Mixins
+	import Head from '~/mixins/Head';
+	import LifecycleHooks from '~/mixins/LifecycleHooks';
+	import Transitions from '~/mixins/Transitions';
+	
+	// Event dispatcher
+	import { Events, TRANSITION_ENTER_DONE } from "~/assets/js/Events";
+	
+	export default {
+		name: 'Contact',
+		mixins: [ Head, LifecycleHooks, Transitions ],
+		computed: {
+			...mapState({
+				vertical: state=>state.scroll.verticalScroll
+			})
+		},
+		methods: {
+			...mapMutations({
+				setScrollActive: "scroll/setActive",
+				setScrollDirection: "scroll/setScrollDirection",
+				setScrollTo: "scroll/updateScrollTo"
+			}),
+			
+			// Enable scroll
+			enableScroll() {
+				this.setScrollDirection(this.vertical);
+				this.setScrollActive(true);
+			},
+			
+			/* MOUNTED -------------------------------------------------------------------------------------------------- */
+			addListeners() {
+				this.enterHandler = this.enableScroll.bind(this);
+				Events.addEventListener(TRANSITION_ENTER_DONE, this.enterHandler);
+			},
+			
+			/* BEFORE DESTROY ------------------------------------------------------------------------------------------- */
+			removeListeners() {
+				Events.removeEventListener(TRANSITION_ENTER_DONE, this.enterHandler);
+			},
+		},
+	};
 </script>
 
 <style src="./contact.scss" lang="scss" scoped></style>
