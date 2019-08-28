7c39c7645e687d1eb57009e25a8695f05bf3ed78
Oleg <zizu19988@gmail.com>
2019-08-28 13:58:57 +0300

    (feature/cursor) Comment and refactor

--- plugins/Mouse.js
+++ plugins/Mouse.js
@@ -2,16 +2,20 @@
 
 export default ({ store }) => {
 	
-	// Move
 	window.addEventListener('mousemove', (e) => {
+		// Show after first pixel moved
 		store.dispatch('mouse/changeMouseVisible', 'show');
+		// Update position while mouse moving
 		store.dispatch('mouse/updateMousePosition', e);
 	});
 	
 	document.addEventListener('mouseleave', (e) => {
+		// Hide after mouse leave document
 		store.dispatch('mouse/changeMouseVisible', 'hide');
 	});
 
+	
+	// Add event listeners for change mouse type
 	const cursorTargets = document.querySelectorAll('._c');
 
 	Array.from(cursorTargets).forEach((item) => {
